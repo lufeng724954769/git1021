0a929b7d5e9b49356db66c5b01b59a78c54b0885
luFeng <wordCount_lu@126.com>
2020-03-29 19:20:02 +0800

    people add2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
+        System.out.println("你好！世界");
         System.out.println("HelloWorld");
         System.out.println("已二次开发");
     }
